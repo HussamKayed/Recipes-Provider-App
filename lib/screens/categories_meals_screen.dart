289c6c0d7706152bc31169779d005093b93bd388
Hussam Kayed <hossamkayed99@gmail.com>
2021-10-28 17:48:10 +0200

    section 167

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:third_flutter_app/dummy_data.dart';
+import 'package:third_flutter_app/widgets/meal_item.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   static const String routeName = "/category_meals";
@@ -20,7 +21,12 @@ class CategoriesMealsScreen extends StatelessWidget {
     return Scaffold(
         body: ListView.builder(
           itemBuilder: (ctx, index) {
-            return Text(categoryMeals[index].title);
+            return MealItem(
+                title: categoryMeals[index].title,
+                imageUrl: categoryMeals[index].imageUrl,
+                affordability: categoryMeals[index].affordability,
+                complexity: categoryMeals[index].complexity,
+                duration: categoryMeals[index].duration);
           },
           itemCount: categoryMeals.length,
         ),
