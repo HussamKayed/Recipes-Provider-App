65922ec8cc79718b9a5d59cc978e33b9262a6ecc
Hussam Kayed <hossamkayed99@gmail.com>
2021-10-29 23:39:51 +0200

    Section 170

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import 'package:third_flutter_app/screens/categories_meals_screen.dart';
+import 'package:third_flutter_app/screens/meal_detail_screen.dart';
 import 'screens/categories_screen.dart';
 
 void main() {
@@ -10,23 +11,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "DeliMeals",
-        theme: ThemeData(
-            primarySwatch: Colors.pink,
-            accentColor: Colors.amber,
-            canvasColor: Color.fromRGBO(255, 254, 229, 1),
-            fontFamily: "Raleway",
-            textTheme: ThemeData.light().textTheme.copyWith(
-                bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-                bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-                headline6: TextStyle(
-                    fontSize: 20,
-                    fontFamily: "RobotoCondensed",
-                    fontWeight: FontWeight.bold))),
-        home: CategoriesScreen(),
-        routes: {
-          CategoriesMealsScreen.routeName: (context) => CategoriesMealsScreen(),
-        });
+      title: "DeliMeals",
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: "Raleway",
+          textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              headline6: TextStyle(
+                  fontSize: 20,
+                  fontFamily: "RobotoCondensed",
+                  fontWeight: FontWeight.bold))),
+      home: CategoriesScreen(),
+      initialRoute: "/",
+      routes: {
+        CategoriesMealsScreen.routeName: (context) => CategoriesMealsScreen(),
+        MealDetailScreen.routeName: (context) => MealDetailScreen(),
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (ctx) => CategoriesScreen(),
+        );
+      },
+    );
   }
 }
 
